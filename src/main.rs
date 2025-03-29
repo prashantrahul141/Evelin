87842aaf9f657ce33a63ab0b40764b550b612c19
prashantrahul141 <prashantrahul141@protonmail.com>
2025-03-30 00:25:15 +0530

    support repl
    Signed-off-by: prashantrahul141 <prashantrahul141@protonmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 mod backend;
 mod cli;
+mod lexer;
+mod token;
 mod utils;
 
-use std::path::{self, Path};
+use std::io::{self, Write};
+use std::{fs, path::Path};
 
 use clap::Parser;
 use cli::ZSCliOptions;
 use env_logger::Env;
-use log::error;
+use lexer::Lexer;
+use log::{error, trace};
 
 fn init() -> ZSCliOptions {
     let cli = ZSCliOptions::parse();
@@ -19,15 +23,41 @@ fn init() -> ZSCliOptions {
     let env = Env::default().filter_or("ZS_LOG_LEVEL", level);
     env_logger::init_from_env(env);
 
-    let file_path = Path::new(&cli.file);
-    if !file_path.is_file() && !file_path.exists() {
-        die!("File not found {}", cli.file.to_str().unwrap());
-    }
+    match &cli.file {
+        cli::InFile::File(f) => {
+            let file_path = Path::new(f);
+            if !file_path.is_file() && !file_path.exists() {
+                die!("File not found {}", f.to_str().unwrap());
+            }
+        }
+        cli::InFile::Stdin => {
+            trace!("repl mode.");
+        }
+    };
 
     cli
 }
 
 fn main() {
     let options = init();
-    let in_file = path::absolute(&options.file).unwrap();
+    match options.file {
+        // file mode.
+        cli::InFile::File(f) => {
+            let in_src = fs::read_to_string(f).unwrap();
+            let mut lexer = Lexer::new(&in_src);
+            lexer.start();
+            dbg!(lexer.tokens);
+        }
+        // repl mode.
+        cli::InFile::Stdin => loop {
+            let mut in_line = String::new();
+            print!(">>> ");
+            io::stdout().flush().unwrap();
+            io::stdin().read_line(&mut in_line).unwrap();
+
+            let mut lexer = Lexer::new(&in_line);
+            lexer.start();
+            dbg!(lexer.tokens);
+        },
+    };
 }
