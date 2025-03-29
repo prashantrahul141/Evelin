420c7d4b877f4ef434ea04cdf476dc7806b70aaa
prashantrahul141 <prashantrahul141@protonmail.com>
2025-03-30 00:25:00 +0530

    support for stdin in cli
    Signed-off-by: prashantrahul141 <prashantrahul141@protonmail.com>

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, str::FromStr};
 
 use clap::Parser;
 
@@ -8,11 +8,29 @@ pub enum DebugTypes {
     Debug,
 }
 
+#[derive(Clone, Debug)]
+pub enum InFile {
+    File(PathBuf),
+    Stdin,
+}
+
+impl FromStr for InFile {
+    type Err = anyhow::Error;
+
+    fn from_str(v: &str) -> Result<InFile, anyhow::Error> {
+        if v == "-" {
+            Ok(InFile::Stdin)
+        } else {
+            Ok(InFile::File(PathBuf::from_str(v).unwrap()))
+        }
+    }
+}
+
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 pub struct ZSCliOptions {
     /// File path
-    pub file: PathBuf,
+    pub file: InFile,
 
     /// Turn debugging information on
     #[clap(value_enum, default_value_t = DebugTypes::Error)]
