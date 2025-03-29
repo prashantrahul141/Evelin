b686450f73b820990186b054c751c43e5cab4b9a
prashantrahul141 <prashantrahul141@protonmail.com>
2025-03-29 12:50:15 +0530

    add: character utils
    Signed-off-by: prashantrahul141 <prashantrahul141@protonmail.com>

--- src/utils.rs
+++ src/utils.rs
@@ -5,3 +5,27 @@ macro_rules! die {
         panic!($($arg)+);
     }
 }
+
+/// Checks whether the given char is ascii alpabetic or _; return True.
+/// else returns False.
+/// # Arguments
+/// * `target_char` - the character to check
+pub fn is_alpha(target_char: char) -> bool {
+    target_char.is_ascii_alphabetic() || target_char == '_'
+}
+
+/// Checks whether the given char is ascii numeric.
+/// else returns False.
+/// # Arguments
+/// * `target_char` - the character to check
+pub fn is_numeric(target_char: char) -> bool {
+    target_char.is_ascii_digit()
+}
+
+/// Checks whether given char is ascii numeric, or ascii alpabetic or _; return True.
+/// else returns False.
+/// # Arguments
+/// * `target_char` - the character to check
+pub fn is_alphanumeric(target_char: char) -> bool {
+    is_alpha(target_char) || is_numeric(target_char)
+}
