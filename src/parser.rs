6238aa2286a2a53ebc21dd041aee89bc23478db8
prashantrahul141 <prashantrahul141@protonmail.com>
2025-03-31 02:27:51 +0530

    feat: add `consume`, `match_current` and `report_parser_error`
    Signed-off-by: prashantrahul141 <prashantrahul141@protonmail.com>

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,4 @@
-use crate::token::Token;
+use crate::token::{Token, TokenType};
 
 pub struct Parser<'a> {
     /// Vec of tokens to parse.
@@ -22,6 +22,17 @@ impl<'a> Parser<'a> {
 
     pub fn parse(&mut self) {}
 
+    /// Consumes current token if it matches the given token type.
+    /// * `expected_type` - type of token to match with.
+    /// * `message` - error message for when token doesn't match the expected type
+    fn consume(&mut self, expected_type: TokenType, message: &str) {
+        if self.match_current(expected_type) {
+            self.advance();
+        }
+
+        self.report_parser_error(message);
+    }
+
     /// Checks whether the current token is
     /// present in the given expected vec of tokens
     /// * `expected` - expected vec of tokens.
@@ -34,6 +45,16 @@ impl<'a> Parser<'a> {
         }
 
         false
+    }
+
+    /// Checks whether the current token is of given type
+    /// * `expected_type` - the type to match with.
+    fn match_current(&self, expected_type: TokenType) -> bool {
+        if self.is_at_end() {
+            return false;
+        }
+
+        self.current().ttype == expected_type
     }
 
     /// Consumes current token and returns it.
@@ -59,5 +80,12 @@ impl<'a> Parser<'a> {
     /// Checks whether the end of the token stream has been reached.
     fn is_at_end(&self) -> bool {
         self.tokens.len() >= self.current
+    }
+
+    /// Sets parser's error flag, then logs the given error message.
+    /// * `message` - error message.
+    fn report_parser_error(&mut self, message: &str) {
+        self.has_errors = true;
+        println!("Parser Error at line {} : {}", self.current().line, message);
     }
 }
