66ebf4437638d98ddddd74baa81e61a102f5e205
prashantrahul141 <prashantrahul141@protonmail.com>
2025-03-31 14:34:06 +0530

    fix: parsing expression
    Signed-off-by: prashantrahul141 <prashantrahul141@protonmail.com>

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,11 @@
-use crate::token::{Token, TokenType};
+use anyhow::anyhow;
+use log::{debug, error, trace};
+
+use crate::{
+    ast::{BinExpr, BinOp, Expr, GroupExpr, LiteralExpr, UnOp, UnaryExpr},
+    die,
+    token::{LiteralValue, Token, TokenType},
+};
 
 pub struct Parser<'a> {
     /// Vec of tokens to parse.
@@ -9,37 +16,171 @@ pub struct Parser<'a> {
 
     /// flag to be set if any error occurs during parsing.
     has_errors: bool,
+
+    /// vec of parsed ast.
+    pub stmts: Vec<Expr>,
 }
 
 impl<'a> Parser<'a> {
     pub fn new(tokens: &'a Vec<Token>) -> Self {
+        debug!("Start parsing--------------------------------");
         Self {
             tokens,
             current: 0,
             has_errors: false,
+            stmts: vec![],
         }
     }
 
-    pub fn parse(&mut self) {}
+    /// Public api to start parsing.
+    pub fn parse(&mut self) {
+        while !self.is_at_end() {
+            trace!("parsing new stmt.");
+            match self.expr() {
+                Ok(v) => self.stmts.push(v),
+                Err(_) => todo!("me when parsing error"),
+            }
+        }
+    }
+
+    /// Parses top-level expressions.
+    fn expr(&mut self) -> Result<Expr, anyhow::Error> {
+        self.term()
+    }
+
+    /// Parses term expressions.
+    fn term(&mut self) -> Result<Expr, anyhow::Error> {
+        if let Ok(left) = self.factor() {
+            while self.match_token(&[TokenType::Minus, TokenType::Plus]) {
+                let op = BinOp::from(&self.previous().ttype);
+                if let Ok(right) = self.factor() {
+                    let bin = BinExpr {
+                        left: left.clone(),
+                        op,
+                        right,
+                    };
+
+                    return Ok(Expr::Binary(Box::new(bin)));
+                }
+            }
+            return Ok(left);
+        }
+
+        let error =
+            self.report_parser_error(format!("Expected '+' or '-' got {}", self.previous()));
+
+        Err(anyhow!(error))
+    }
+
+    /// Parses factor expressions.
+    fn factor(&mut self) -> Result<Expr, anyhow::Error> {
+        if let Ok(left) = self.unary() {
+            while self.match_token(&[TokenType::Slash, TokenType::Star, TokenType::Mod]) {
+                let op = BinOp::from(&self.previous().ttype);
+                if let Ok(right) = self.unary() {
+                    let bin = BinExpr {
+                        left: left.clone(),
+                        op,
+                        right,
+                    };
+
+                    return Ok(Expr::Binary(Box::new(bin)));
+                }
+            }
+
+            return Ok(left);
+        }
+
+        let error =
+            self.report_parser_error(format!("Expected '/', '*' or '%' got {}", self.previous()));
+
+        Err(anyhow!(error))
+    }
+
+    /// Parses unary expressions.
+    fn unary(&mut self) -> Result<Expr, anyhow::Error> {
+        if self.match_token(&[TokenType::Bang, TokenType::Minus]) {
+            let op = UnOp::from(&self.previous().ttype);
+            if let Ok(operand) = self.unary() {
+                let un = UnaryExpr { op, operand };
+                return Ok(Expr::Unary(Box::new(un)));
+            }
+        }
+
+        let primary = self.primary();
+        trace!("Parser::primary = {:?}", primary);
+        primary
+    }
+
+    /// Parses literal expressions.
+    fn primary(&mut self) -> Result<Expr, anyhow::Error> {
+        if self.match_token(&[TokenType::False]) {
+            let literal = Expr::Literal(LiteralExpr {
+                value: LiteralValue::Boolean(false),
+            });
+
+            return Ok(literal);
+        }
+
+        if self.match_token(&[TokenType::True]) {
+            let literal = Expr::Literal(LiteralExpr {
+                value: LiteralValue::Boolean(true),
+            });
+
+            return Ok(literal);
+        }
+
+        // literal values get parsed in the lexer section, we can just clone it here.
+        if self.match_token(&[
+            TokenType::Null,
+            TokenType::String,
+            TokenType::NumberInt,
+            TokenType::NumberFloat,
+        ]) {
+            let literal = Expr::Literal(LiteralExpr {
+                value: self.previous().literal.clone(),
+            });
+
+            return Ok(literal);
+        }
+
+        // grouping
+        if self.match_token(&[TokenType::LeftParen]) {
+            trace!("Grouping found.");
+            if let Ok(expr) = self.expr() {
+                self.consume(TokenType::RightParen, "Expected ')'");
+                let literal = Expr::Grouping(Box::new(GroupExpr { value: expr }));
+                return Ok(literal);
+            }
+        }
+
+        let error = self.report_parser_error(format!(
+            "Expected literal value recieved {} instead.",
+            self.current().literal
+        ));
+
+        Err(anyhow!(error))
+    }
 
     /// Consumes current token if it matches the given token type.
     /// * `expected_type` - type of token to match with.
     /// * `message` - error message for when token doesn't match the expected type
     fn consume(&mut self, expected_type: TokenType, message: &str) {
-        if self.match_current(expected_type) {
+        if self.match_current(&expected_type) {
             self.advance();
         }
 
         self.report_parser_error(message);
+        die!("{}", message);
     }
 
     /// Checks whether the current token is
     /// present in the given expected vec of tokens
     /// * `expected` - expected vec of tokens.
-    fn match_token(&self, expected: &[Token]) -> bool {
-        let current = self.current();
-        for token in expected {
-            if token.ttype == current.ttype {
+    fn match_token(&mut self, expected: &[TokenType]) -> bool {
+        for ttype in expected {
+            if self.match_current(ttype) {
+                self.advance();
                 return true;
             }
         }
@@ -49,12 +190,12 @@ impl<'a> Parser<'a> {
 
     /// Checks whether the current token is of given type
     /// * `expected_type` - the type to match with.
-    fn match_current(&self, expected_type: TokenType) -> bool {
+    fn match_current(&self, expected_type: &TokenType) -> bool {
         if self.is_at_end() {
             return false;
         }
 
-        self.current().ttype == expected_type
+        expected_type == &self.current().ttype
     }
 
     /// Consumes current token and returns it.
@@ -64,7 +205,9 @@ impl<'a> Parser<'a> {
             self.current += 1;
         }
 
-        self.previous()
+        let prev = self.previous();
+        trace!("Parser::advance self.previous() = {}", &prev);
+        prev
     }
 
     /// Returns current token.
@@ -74,18 +217,28 @@ impl<'a> Parser<'a> {
 
     /// Returns previous token.
     fn previous(&self) -> &Token {
+        if self.current == 0 {
+            die!("Parser::previous self.current = 0");
+        }
         &self.tokens[self.current - 1]
     }
 
     /// Checks whether the end of the token stream has been reached.
     fn is_at_end(&self) -> bool {
-        self.tokens.len() >= self.current
+        self.current().ttype == TokenType::Eof
     }
 
     /// Sets parser's error flag, then logs the given error message.
     /// * `message` - error message.
-    fn report_parser_error(&mut self, message: &str) {
+    fn report_parser_error<T: Into<String>>(&mut self, message: T) -> String {
         self.has_errors = true;
-        println!("Parser Error at line {} : {}", self.current().line, message);
+        let value: String = message.into();
+        error!(
+            "Parsing error: at line {}: {:?}",
+            self.previous().line,
+            value
+        );
+
+        value
     }
 }
