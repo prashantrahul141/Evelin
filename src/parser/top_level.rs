0e0b00c4a216c0f96f0cdf730bf9fbdd594fc940
prashantrahul141 <prashantrahul141@protonmail.com>
2025-04-06 14:13:18 +0530

    feat: add top level parsing functions for fn and struct declaration
    Signed-off-by: prashantrahul141 <prashantrahul141@protonmail.com>

---
+++ src/parser/top_level.rs
@@ -0,0 +1,61 @@
+use anyhow::anyhow;
+
+use crate::{
+    ast::{FnDecl, Stmt, StructDecl},
+    token::TokenType,
+};
+
+use super::{ParserResult, parser::Parser};
+
+impl<'a> Parser<'a> {
+    pub(super) fn fn_decl(&mut self) -> ParserResult<FnDecl> {
+        let name = match self.consume(TokenType::Identifier, "Expected function name.") {
+            Some(v) => v.lexeme.clone(),
+            None => return Err(anyhow!("Expected function name,")),
+        };
+
+        self.consume(TokenType::LeftParen, "Expected '(' after function name");
+        let mut parameter = None;
+        if self.match_current(&TokenType::RightParen) {
+            parameter = Some(self.current().lexeme.clone());
+        }
+
+        self.consume(
+            TokenType::RightParen,
+            "Expected ')' after function parameter",
+        );
+        let body = match self.block()? {
+            Stmt::Block(block) => block.stmts,
+            _ => return Err(anyhow!("Expected block after function declaration")),
+        };
+
+        Ok(FnDecl {
+            name,
+            parameter,
+            body,
+        })
+    }
+
+    pub(super) fn struct_decl(&mut self) -> ParserResult<StructDecl> {
+        let name = match self.consume(TokenType::Identifier, "Expected struct name.") {
+            Some(v) => v.lexeme.clone(),
+            None => return Err(anyhow!("Expected struct name,")),
+        };
+
+        self.consume(TokenType::LeftBrace, "Expected '{' after struct name");
+
+        let mut fields = vec![];
+        while !self.match_token(&[TokenType::RightBrace]) && !self.is_at_end() {
+            match self.consume(TokenType::Identifier, "Expected field name") {
+                Some(f) => fields.push(f.lexeme.clone()),
+                None => return Err(anyhow!("Expected field name")),
+            };
+
+            if !self.match_current(&TokenType::RightBrace) {
+                self.consume(TokenType::Comma, "Expected commo after field name");
+            }
+        }
+
+        Ok(StructDecl { name, fields })
+    }
+}
