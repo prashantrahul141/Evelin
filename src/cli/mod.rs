879503fe0a1e168a1b8f645e2d5972b5f16812c5
prashantrahul141 <prashantrahul141@protonmail.com>
2025-04-08 19:28:59 +0530

    fix: use clap to display error when input file is absent
    Signed-off-by: prashantrahul141 <prashantrahul141@protonmail.com>

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -2,9 +2,13 @@ use std::io::Write;
 use std::path::Path;
 use std::path::PathBuf;
 
-use anyhow::bail;
+use clap::CommandFactory;
 use clap::Parser;
+use clap::error::ErrorKind;
+use colored::Colorize;
 use env_logger::{Builder, Env};
+
+const EVE_FILE_EXTENSION: &str = "eve";
 
 #[derive(clap::ValueEnum, Clone, Debug)]
 pub enum DebugTypes {
@@ -14,10 +18,10 @@ pub enum DebugTypes {
 }
 
 #[derive(Parser)]
-#[command(version, about, long_about = None)]
+#[command(version, about= "The Evelin Programming Language", long_about = None)]
 pub struct EveCliOptions {
-    /// Evelin source file path
-    pub file: PathBuf,
+    /// Evelin source files path
+    pub file: Vec<PathBuf>,
 
     /// Turn debugging information on
     #[clap(value_enum, default_value_t = DebugTypes::Error)]
@@ -27,6 +31,8 @@ pub struct EveCliOptions {
 
 pub fn init() -> anyhow::Result<EveCliOptions> {
     let cli = EveCliOptions::parse();
+    let mut cmd = EveCliOptions::command();
+
     let level = match cli.debug {
         DebugTypes::Error => "error",
         DebugTypes::Debug => "debug",
@@ -49,9 +55,34 @@ pub fn init() -> anyhow::Result<EveCliOptions> {
         })
         .init();
 
-    let file_path = Path::new(&cli.file);
-    if !file_path.is_file() || !file_path.exists() {
-        bail!("File '{}' not found", cli.file.to_str().unwrap());
+    for f in &cli.file {
+        let file_path = Path::new(f);
+
+        let ext_matches = file_path
+            .extension()
+            .and_then(|ext| ext.to_str())
+            .map_or(false, |ext| ext == EVE_FILE_EXTENSION);
+
+        let tmp_str = f.to_str().unwrap();
+        if !file_path.is_file() || !file_path.exists() {
+            cmd.error(
+                ErrorKind::ValueValidation,
+                format!("File '{}' not found.", tmp_str),
+            )
+            .exit();
+        }
+
+        if !ext_matches {
+            cmd.error(
+                ErrorKind::ValueValidation,
+                format!(
+                    "Incorrect file type for {}. Expected a {} file.",
+                    tmp_str.red(),
+                    ".eve".green()
+                ),
+            )
+            .exit();
+        }
     }
 
     Ok(cli)
