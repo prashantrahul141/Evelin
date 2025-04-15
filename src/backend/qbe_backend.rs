d937228c0b14fd3442f691dfaed089eb2362fb42
prashantrahul141 <prashantrahul141@protonmail.com>
2025-04-15 22:07:28 +0530

    feat: use statically link qbe binary
    Signed-off-by: prashantrahul141 <prashantrahul141@protonmail.com>

--- src/backend/qbe_backend.rs
+++ src/backend/qbe_backend.rs
@@ -1,74 +1,63 @@
 use std::{
-    fs::File,
-    io::Write,
-    os::unix::fs::PermissionsExt,
-    process::{Command, Stdio},
+    ffi::{CStr, CString},
+    ptr,
 };
 
 use anyhow::{Context, bail};
 
 use super::Backend;
 
-const QBE_BINARY_DATA: &[u8] = include_bytes!("../../external/qbe-1.2/qbe");
-
-#[cfg(target_os = "macos")]
-const QBE_EXECUTABLE_PATH: &str = "./qbe";
-
-#[cfg(target_os = "linux")]
-const QBE_EXECUTABLE_PATH: &str = "/tmp/qbe";
+#[link(name = "qbe")]
+unsafe extern "C" {
+    fn qbe(input_fp: *mut libc::FILE, output_fp: *mut libc::FILE) -> libc::c_void;
+}
 
 /// Public qbe backend struct.
 pub struct QbeBackend;
 
 impl QbeBackend {
     pub fn new() -> anyhow::Result<Self> {
-        let mut exe_file = File::create(QBE_EXECUTABLE_PATH)
-            .context(format!("Failed to create {} file", QBE_EXECUTABLE_PATH))?;
-
-        exe_file
-            .write_all(QBE_BINARY_DATA)
-            .context(format!("Failed to write to {} file", QBE_EXECUTABLE_PATH))?;
-
-        let metadata = exe_file.metadata()?;
-        let mut permission = metadata.permissions();
-        permission.set_mode(0o722);
-        std::fs::set_permissions(QBE_EXECUTABLE_PATH, permission.clone()).with_context(|| {
-            format!(
-                "Failed to elevate qbe binary file permission: {:?}",
-                permission
-            )
-        })?;
         Ok(Self {})
     }
 }
 
 impl Backend for QbeBackend {
     fn generate(&self, ir: String) -> Result<String, anyhow::Error> {
-        let mut child = Command::new(QBE_EXECUTABLE_PATH)
-            .stdin(Stdio::piped())
-            .stdout(Stdio::piped())
-            .stderr(Stdio::piped())
-            .spawn()
-            .context("Failed to spawn child qbe process.")?;
+        let input = CString::new(ir).context("Failed to create C string from IR")?;
+        let size = input.as_bytes().len();
+        unsafe {
+            let input_fp: *mut libc::FILE = libc::fmemopen(
+                input.as_ptr() as *mut libc::c_void,
+                size,
+                CString::new("r")?.as_ptr(),
+            );
 
-        let mut stdin = child
-            .stdin
-            .take()
-            .context("Failed to open stdin for qbe child process.")?;
+            if input_fp.is_null() {
+                bail!("Failed to create in-memory FILE*");
+            }
 
-        std::thread::spawn(move || -> anyhow::Result<()> {
-            stdin
-                .write_all(ir.as_bytes())
-                .context("Failed to write to stdin for qbe child process.")
-        });
+            let mut output_ptr: *mut libc::c_char = ptr::null_mut();
+            let mut output_size: libc::size_t = 0;
 
-        let output = child.wait_with_output()?;
+            let output_fp: *mut libc::FILE =
+                libc::open_memstream(&mut output_ptr, &mut output_size);
+            if output_fp.is_null() {
+                bail!("Failed to create output FILE*");
+            }
 
-        let err = String::from_utf8(output.stderr)?;
-        if !err.is_empty() {
-            bail!(format!("Qbe error : {}", err));
+            // call qbe with input_fp, output_fp
+            qbe(input_fp, output_fp);
+
+            libc::fflush(output_fp);
+            libc::fclose(input_fp);
+            libc::fclose(output_fp);
+
+            let c_str = CStr::from_ptr(output_ptr);
+            let rs_out = c_str.to_string_lossy().into_owned();
+
+            libc::free(output_ptr as *mut libc::c_void);
+
+            return Ok(rs_out);
         }
-
-        Ok(String::from_utf8(output.stdout)?)
     }
 }
