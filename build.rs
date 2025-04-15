72c6966aa903875d58c0fccb413d24dfa83a64e8
prashantrahul141 <prashantrahul141@protonmail.com>
2025-04-15 22:06:50 +0530

    feat: link to qbe
    Signed-off-by: prashantrahul141 <prashantrahul141@protonmail.com>

--- build.rs
+++ build.rs
@@ -1,8 +1,6 @@
 use std::process::Command;
 
-const QBE_ROOT_DIR: &str = "./external/qbe-1.2/";
-const EXTERNAL_ROOT_DIR: &str = "./external/";
-const QBE_URI: &str = "https://c9x.me/compile/release/qbe-1.2.tar.xz";
+const QBE_ROOT_DIR: &str = "./external/qbe/";
 
 macro_rules! p {
     ($($tokens: tt)*) => {
@@ -11,25 +9,7 @@ macro_rules! p {
 }
 
 fn main() {
-    println!("cargo::rerun-if-changed=external/qbe-1.2/main.c");
     p!("Rerun build.");
-
-    if let Err(e) = Command::new("wget")
-        .args(["-nc", QBE_URI])
-        .current_dir(EXTERNAL_ROOT_DIR)
-        .output()
-    {
-        panic!("Failed to download qbe tar file : {}", e);
-    }
-
-    if let Err(e) = Command::new("tar")
-        .args(["vxf", "qbe-1.2.tar.xz"])
-        .current_dir(EXTERNAL_ROOT_DIR)
-        .output()
-    {
-        panic!("Failed to untar qbe file: {}", e);
-    }
-
     if let Err(e) = Command::new("make")
         .args(["-j", "16"])
         .current_dir(QBE_ROOT_DIR)
@@ -37,4 +17,7 @@ fn main() {
     {
         panic!("Failed while building qbe : {}", e);
     }
+
+    println!("cargo:rustc-link-search=external/qbe/");
+    println!("cargo:rustc-link-lib=qbe");
 }
