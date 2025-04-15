7e539b558efbaeade522f707169bc5fd04b845cd
prashantrahul141 <prashantrahul141@protonmail.com>
2025-04-15 16:34:58 +0530

    fix: check for qbe errors in backend
    Signed-off-by: prashantrahul141 <prashantrahul141@protonmail.com>

--- src/backend/qbe_backend.rs
+++ src/backend/qbe_backend.rs
@@ -5,7 +5,7 @@ use std::{
     process::{Command, Stdio},
 };
 
-use anyhow::Context;
+use anyhow::{Context, bail};
 
 use super::Backend;
 
@@ -47,6 +47,7 @@ impl Backend for QbeBackend {
         let mut child = Command::new(QBE_EXECUTABLE_PATH)
             .stdin(Stdio::piped())
             .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
             .spawn()
             .context("Failed to spawn child qbe process.")?;
 
@@ -62,6 +63,12 @@ impl Backend for QbeBackend {
         });
 
         let output = child.wait_with_output()?;
+
+        let err = String::from_utf8(output.stderr)?;
+        if !err.is_empty() {
+            bail!(format!("Qbe error : {}", err));
+        }
+
         Ok(String::from_utf8(output.stdout)?)
     }
 }
