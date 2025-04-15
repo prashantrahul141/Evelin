004f0042a154d8c1f995ee13abc85ba26cdd5c4e
prashantrahul141 <prashantrahul141@protonmail.com>
2025-04-15 10:34:44 +0530

    feat: emit struct meta data while emitting struct
    Signed-off-by: prashantrahul141 <prashantrahul141@protonmail.com>

--- src/emitter/qbe/data.rs
+++ src/emitter/qbe/data.rs
@@ -1,18 +1,48 @@
-use log::debug;
+use std::{cmp, collections::HashMap};
 
 use crate::ast::StructDecl;
+use log::debug;
 
 use super::QBEEmitter;
+
+/// mapping of field -> (type, offset)
+pub(super) type StructMeta = HashMap<String, (qbe::Type<'static>, u64)>;
 
 impl QBEEmitter<'_> {
     /// Emits a single function
     pub(super) fn emit_data_def(&mut self, struct_decl: &StructDecl) {
         self.tmp_counter += 1;
-        let typedef = qbe::TypeDef {
+        let mut typedef = qbe::TypeDef {
             name: struct_decl.name.clone(),
             align: None,
             items: vec![],
         };
+
+        let mut meta = StructMeta::new();
+        let mut offset = 0;
+        let mut max_align = 0;
+
+        for field in &struct_decl.fields {
+            let ty = qbe::Type::try_from(field.1.clone()).unwrap();
+            let field_align = Self::type_alignment(&ty);
+            max_align = cmp::max(max_align, field_align);
+
+            // align with the current offset
+            offset = Self::align_offset(offset, field_align);
+
+            meta.insert(field.0.clone(), (ty.clone(), offset));
+            typedef.items.push((ty.clone(), 1));
+
+            offset += ty.size();
+        }
+
+        offset = Self::align_offset(offset, max_align);
+
+        // set typedef's alignment
+        typedef.align = Some(max_align);
+
+        self.struct_meta
+            .insert(struct_decl.name.clone(), (meta, offset));
 
         self.module.add_type(typedef);
     }
