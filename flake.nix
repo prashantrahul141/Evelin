78185b106546cf0ff7c10f8faef104231b96f838
prashantrahul141 <prashantrahul141@protonmail.com>
2025-04-20 22:16:32 +0530

    refactor: move external/* to vendor/*
    Signed-off-by: prashantrahul141 <prashantrahul141@protonmail.com>

--- flake.nix
+++ flake.nix
@@ -1,37 +1,39 @@
 {
-  description = "dev environment for evelin programming language";
+  lib,
+  rustPlatform,
+  pkgs,
+}:
+{
+  bro = rustPlatform.buildRustPackage rec {
+    pname = "evelin";
+    version = "0.0.1";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?rev=eb0e0f21f15c559d2ac7633dc81d079d1caf5f5f";
+    src = ./.;
+
+    cargoLock = {
+      lockFile = ./Cargo.lock;
+      allowBuiltinFetchGit = true;
+    };
+    meta = {
+      description = "bro";
+      homepage = "https://github.com/BurntSushi/ripgrep";
+      license = lib.licenses.unlicense;
+      maintainers = [ ];
+    };
+
+    nativeBuildInputs = with pkgs; [
+      rustPlatform.cargoSetupHook
+      setuptools-rust
+      cargo
+      rustc
+      rustfmt
+      rust-analyzer
+      rustPackages.clippy
+      binutils
+      gnumake
+      wget
+      gnutar
+    ];
   };
 
-  outputs =
-    { self, nixpkgs }:
-    let
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-      forAllSystems =
-        f: nixpkgs.lib.genAttrs supportedSystems (system: f nixpkgs.legacyPackages.${system});
-    in
-    {
-      devShells = forAllSystems (pkgs: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            cargo
-            rustc
-            rustfmt
-            rust-analyzer
-            rustPackages.clippy
-            binutils
-            gnumake
-            wget
-            gnutar
-          ];
-        };
-      });
-    };
 }
