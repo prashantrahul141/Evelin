4d42915fe2946139fa9af1d547c03fa4a461de87
prashantrahul141 <prashantrahul141@protonmail.com>
2025-04-27 17:06:25 +0530

    refactor: accumulate all fn_decls and struct_decls from different files
    Signed-off-by: prashantrahul141 <prashantrahul141@protonmail.com>

--- src/main.rs
+++ src/main.rs
@@ -25,9 +25,11 @@ pub fn init() -> anyhow::Result<()> {
 
     let opts = cli::init()?;
 
-    let mut out_files = vec![];
-    for f in opts.file {
-        let in_src = fs::read_to_string(&f).context("Failed to read input file")?;
+    let mut fn_decls = vec![];
+    let mut struct_decls = vec![];
+
+    for f in &opts.file {
+        let in_src = fs::read_to_string(f).context("Failed to read input file")?;
 
         let mut lexer = lexer::Lexer::from(&in_src);
         lexer.start()?;
@@ -44,45 +46,44 @@ pub fn init() -> anyhow::Result<()> {
             );
         }
 
-        let (fn_, st) = match passes::run_passes(parser.fn_decls, parser.struct_decls) {
-            Ok((fn_, st)) => (fn_, st),
-            Err(errs) => {
-                for e in &errs {
-                    report_message(e.to_string(), MessageType::Error);
-                }
-                bail!("Failed to compile due to {} error(s)", &errs.len());
-            }
-        };
-
-        let mut qbe_generator = QBEEmitter::from((&fn_, &st));
-        let ir = qbe_generator.emit_ir()?;
-        debug!("IR: \n{}", ir);
-
-        let backend = QbeBackend {};
-        let obj_code = backend.generate(ir)?;
-        debug!("OBJ_CODE: \n{}", obj_code);
-
-        let mut abs_outfile =
-            std::path::absolute(f).context("Failed to get absolute path of the input file")?;
-        abs_outfile.set_extension("s");
-        fs::write(&abs_outfile, obj_code).context("Failed to write qbe output to a file")?;
-        out_files.push(abs_outfile);
+        fn_decls.append(&mut parser.fn_decls);
+        struct_decls.append(&mut parser.struct_decls);
     }
+
+    let (fn_decls, struct_decls) = match passes::run_passes(fn_decls, struct_decls) {
+        Ok((fn_, st)) => (fn_, st),
+        Err(errs) => {
+            for e in &errs {
+                report_message(e.to_string(), MessageType::Error);
+            }
+            bail!("Failed to compile due to {} error(s)", &errs.len());
+        }
+    };
+
+    let mut qbe_generator = QBEEmitter::from((&fn_decls, &struct_decls));
+    let ir = qbe_generator.emit_ir()?;
+    debug!("IR: \n{}", ir);
+
+    let backend = QbeBackend {};
+    let obj_code = backend.generate(ir)?;
+    debug!("OBJ_CODE: \n{}", obj_code);
+
+    let mut abs_outfile = std::path::absolute(opts.file.first().unwrap())?;
+    abs_outfile.set_extension("s");
+    fs::write(&abs_outfile, obj_code).context("Failed to write qbe output to a file")?;
 
     // build executable using platform's c compiler
     let out = cc_runtime::Build::default()
         .set_c_compiler(opts.cc)
-        .files(&out_files)
+        .file(&abs_outfile)
         .set_outfile(&opts.out)
         .set_lib_paths(opts.lib_path.unwrap_or(vec![]))
         .set_lib_names(opts.lib_name.unwrap_or(vec![]))
         .set_opt(3)
         .compile()?;
 
-    // delete temporary files.
-    for file in out_files {
-        fs::remove_file(file)?;
-    }
+    // delete temporary file.
+    fs::remove_file(&abs_outfile)?;
 
     let elapsed_time = initial_time.elapsed();
     if !out.stderr.is_empty() {
--- src/passes/main_fn_existence.rs
+++ src/passes/main_fn_existence.rs
@@ -8,11 +8,10 @@ pub struct MainFnExistence {}
 
 impl EvePass for MainFnExistence {
     fn run_pass(&self, fn_decls: Vec<FnDecl>, st_decl: Vec<StructDecl>) -> PassResult {
-        for fns in &fn_decls {
-            if fns.name == "main" {
-                return Ok((fn_decls, st_decl));
-            }
+        if fn_decls.iter().find(|fns| fns.name == "main").is_some() {
+            return Ok((fn_decls, st_decl));
         }
+
         Err(vec![anyhow!("No main function found.")])
     }
 }
