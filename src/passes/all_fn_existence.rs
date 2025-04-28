51584d59606fb17ac90c4cbc9e4f7c810750dff1
prashantrahul141 <prashantrahul141@protonmail.com>
2025-04-28 19:57:21 +0530

    feat: add all_fn_existence pass
    Signed-off-by: prashantrahul141 <prashantrahul141@protonmail.com>

---
+++ src/passes/all_fn_existence.rs
@@ -0,0 +1,38 @@
+use anyhow::anyhow;
+
+use crate::ast::{CallExpr, Expr, FnDecl, Stmt, StructDecl};
+
+use super::{EvePass, PassResult, PassResultGeneric};
+
+pub struct AllFnExistence {}
+
+impl EvePass for AllFnExistence {
+    fn run_pass(&self, fn_decls: Vec<FnDecl>, st_decl: Vec<StructDecl>) -> PassResult {
+        for fns in &fn_decls {
+            for stmt in &fns.body {
+                if let Stmt::Expression(Expr::Call(call)) = stmt {
+                    self.check_fn_existence(&fn_decls, call)?;
+                }
+            }
+        }
+
+        Ok((fn_decls, st_decl))
+    }
+}
+
+impl AllFnExistence {
+    fn check_fn_existence(&self, fn_decls: &[FnDecl], call: &CallExpr) -> PassResultGeneric<()> {
+        let mut err = vec![];
+        match &call.callee {
+            Expr::Variable(var) => {
+                if fn_decls.iter().any(|decl| decl.name == var.name) {
+                    return Ok(());
+                } else {
+                    err.push(anyhow!("Call to undefined function '{}'", var.name));
+                }
+            }
+            _ => unreachable!(),
+        };
+        Err(err)
+    }
+}
--- src/passes/mod.rs
+++ src/passes/mod.rs
@@ -1,12 +1,12 @@
+use all_fn_existence::AllFnExistence;
 use main_fn_existence::MainFnExistence;
-use return_type::ReturnType;
 use struct_field_missing_and_unknown::StructFieldMissingAndUnknown;
 use struct_init_unique_fields::StructInitUniqueField;
 
 use crate::ast::{FnDecl, StructDecl};
 
+mod all_fn_existence;
 mod main_fn_existence;
-mod return_type;
 mod struct_field_missing_and_unknown;
 mod struct_init_unique_fields;
 
@@ -21,12 +21,12 @@ pub fn run_passes(fn_: Vec<FnDecl>, st_: Vec<StructDecl>) -> PassResult {
     let p = MainFnExistence {};
     let (fn_, st_) = p.run_pass(fn_, st_)?;
 
+    let p = AllFnExistence {};
+    let (fn_, st_) = p.run_pass(fn_, st_)?;
+
     let p = StructInitUniqueField {};
     let (fn_, st_) = p.run_pass(fn_, st_)?;
 
     let p = StructFieldMissingAndUnknown {};
-    let (fn_, st) = p.run_pass(fn_, st_)?;
-
-    let p = ReturnType {};
-    p.run_pass(fn_, st)
+    p.run_pass(fn_, st_)
 }
