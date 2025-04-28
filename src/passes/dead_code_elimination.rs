4d1d7a30247e878dd941ca2a040c8a0ea8eecbc2
prashantrahul141 <prashantrahul141@protonmail.com>
2025-04-28 22:24:46 +0530

    refactor: dead code elimination pass cleanup
    Signed-off-by: prashantrahul141 <prashantrahul141@protonmail.com>

--- src/passes/dead_code_elimination.rs
+++ src/passes/dead_code_elimination.rs
@@ -11,7 +11,7 @@ pub struct DeadCodeElimination {}
 
 impl EvePass for DeadCodeElimination {
     fn run_pass(&self, mut fn_decls: Vec<FnDecl>, st_decl: Vec<StructDecl>) -> PassResult {
-        for fns in &mut fn_decls {
+        for fns in fn_decls.iter_mut().filter(|x| x.name != "main") {
             self.remove_stmt_after_return(fns);
         }
 
@@ -21,14 +21,9 @@ impl EvePass for DeadCodeElimination {
 
 impl DeadCodeElimination {
     fn remove_stmt_after_return(&self, fns: &mut FnDecl) {
-        let mut index = fns.body.len() - 1;
-        for (i, stmt) in fns.body.iter().enumerate() {
-            if matches!(stmt, Stmt::Return(_)) {
-                index = i;
-            }
-        }
+        let index = fns.body.iter().position(|x| matches!(x, &Stmt::Return(_)));
 
-        if index != fns.body.len() - 1 {
+        if let Some(i) = index.filter(|&i| i != fns.body.len() - 1) {
             report_message(
                 format!(
                     "Code after return statement in function '{}' will be ignored",
@@ -36,7 +31,7 @@ impl DeadCodeElimination {
                 ),
                 MessageType::Warning,
             );
-            fns.body.truncate(index);
+            fns.body.truncate(i);
         }
     }
 }
